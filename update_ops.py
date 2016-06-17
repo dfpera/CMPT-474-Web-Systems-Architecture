92a3f5a21452a188b0c9dd685ec0fb196d0b2100
Shixiao Zhang <shixiaoz@sfu.ca>
2016-06-17 08:51:16 -0700

    Refine update_ops

--- update_ops.py
+++ update_ops.py
@@ -4,35 +4,42 @@ from boto.dynamodb2.items import Item
 from boto.dynamodb2.exceptions import ItemNotFound
 
 def add_activity(table, id, activity, response):
-    try:
-        item = table.get_item(id=id)
-        response.status = 200
-        for x in item['activities']:
-            if x == activity:
-                return {
-                            "data": {
-                                "type": "person",
-                                "id": id, # "Replace with actual id"
-                                "added": [] # "[] if no activity added"
-                            }
-                        }
-        item['activities'].append(activity)
-        item.parial_save()
-        return {
-                    "data": {
-                        "type": "person",
-                        "id": id, # "Replace with actual id"
-                        "added": ["<activity>"] # "[] if no activity added"
-                    }
-                }
+  try:
+    item = table.get_item(id=id)
+    response.status = 200
+    if item['activities'] != None:
+      for x in item['activities']:
+        if x == activity:
+          return {"data": {
+          "type": "person",
+          "id": id,
+          "added": []
+          }
+          }
+      item['activities'].add(activity)
+      item.save()
+      return {"data": {
+      "type": "person",
+      "id": id,
+      "added": [activity]
+      }
+      }
 
-	except ItemNotFound as inf:
+    p = Item(table, data={'id': id, 'name': item['name'], 'activities': {activity}})
+    p.save(overwrite=True)
+    return {"data": {
+    "type": "person",
+    "id": id,
+    "added": []
+    }
+    }
+
+  except ItemNotFound as inf:
         response.status = 404
-
         return {
-                    "errors": [{
-                        "not_found": {
-                            "id": id # "Replace with actual id"
-                        }
-                    }]
-                }
+        "errors": [{
+        "not_found": {
+        "id": id # "Replace with actual id"
+        }
+        }]
+        }
