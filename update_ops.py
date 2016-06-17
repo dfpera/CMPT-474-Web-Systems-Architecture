271a7020c5596ab53051f746c330d392fd63bb2d
Shixiao Zhang <shixiaoz@sfu.ca>
2016-06-16 17:52:24 -0700

    Refine update_ops

--- update_ops.py
+++ update_ops.py
@@ -1,8 +1,37 @@
 ''' Add_activities function for CMPT 474 Assignment 2 '''
 
+from boto.dynamodb2.items import Item
+from boto.dynamodb2.exceptions import ItemNotFound
+
 def add_activity(table, id, activity, response):
-    print "Add activity not yet implemented"
-    response.status = 501
-    return {"errors": [{
-        "add activity not implemented": {"id": id, "activity": activity}
-        }]}
+    try:
+        item = table.get_item(id=id)
+        response.status = 200
+        for x in item['activities']:
+            if x == activity:
+                return {
+                            "data": {
+                                "type": "person",
+                                "id": id, # "Replace with actual id"
+                                "added": [] # "[] if no activity added"
+                            }
+                        }
+        item['activities'].append(activity)
+        item.parial_save()
+        return {
+                    "data": {
+                        "type": "person",
+                        "id": id, # "Replace with actual id"
+                        "added": ["<activity>"] # "[] if no activity added"
+                    }
+                }
+
+
+        response.status = 404
+        return {
+                    "errors": [{
+                        "not_found": {
+                            "id": id # "Replace with actual id"
+                        }
+                    }]
+                }
